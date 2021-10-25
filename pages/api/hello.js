602f9a0e1286e0075d1eebf8319d2882fe483d85
William Li <email@williamli.dev>
2021-10-25 16:59:44 +0800

    return demo date object in /api/hello

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,12 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  const start = '2022-02-05T23:00:00.000Z'
+
+  const aDate = new Date(start).toLocaleDateString("de-DE", {
+  year: "numeric",
+  month: "2-digit",
+  day: "2-digit",
+  })
+  res.status(200).json({ date: aDate })
 }
